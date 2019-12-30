44c33de222a628efded4a7d9d08440d975b4bae0
tmi <tmi@pesto.local>
2019-12-30 13:23:11 +0100

    Signed-off-by: tmi <tmi@pesto.local>
    Neue ReleaseExe erstellt

--- Farbenauslesen/Farbenauslesen/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : Quelldatei, die nur die Standard-Includes einbindet.
-// Farbenauslesen.pch ist der vorkompilierte Header.
-// stdafx.obj enthält die vorkompilierten Typinformationen.
-
-#include "stdafx.h"
-
-
